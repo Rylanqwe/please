dfc40ebe28de70ced39e597718bad61eeefbb51d
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-02-05 11:35:27 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const flipBtn = document.getElementById("flipBtn");
+const connectBtn = document.getElementById("connectBtn");
+
+flipBtn.addEventListener("click", function() {
+  alert("Flip button clicked!");
+});
+
+connectBtn.addEventListener("click", function() {
+  alert("Connect button clicked!");
+});
