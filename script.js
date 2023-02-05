9df8861007ed6ef785f4ef00964bef41912b7a83
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-02-05 12:28:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const flipBtn = document.getElementById("flipBtn");
+const connectButton = document.getElementById("connect");
+const flipButton = document.getElementById("flip");
 const resultDiv = document.getElementById("result");
-const connectBtn = document.getElementById("connectBtn");
 
-flipBtn.addEventListener("click", () => {
-  const flipResult = Math.floor(Math.random() * 2) + 1;
-  resultDiv.innerHTML = flipResult === 1 ? "Heads" : "Tails";
+connectButton.addEventListener("click", async () => {
+  try {
+    // Connect to Phantom wallet
+    const connection = await solana.connect();
+
+    // Check if connection was successful
+    if (connection) {
+      resultDiv.innerHTML = "Successfully connected to Phantom wallet";
+    } else {
+      resultDiv.innerHTML = "Failed to connect to Phantom wallet";
+    }
+  } catch (error) {
+    resultDiv.innerHTML = "An error occurred while connecting to Phantom wallet";
+    console.error(error);
+  }
 });
 
-connectBtn.addEventListener("click", async () => {
-  try {
-    const connection = await Solana.connect();
-    console.log("Connected to Solana wallet:", connection);
-  } catch (error) {
-    console.error("Error connecting to Solana wallet:", error);
-  }
+flipButton.addEventListener("click", () => {
+  const result = Math.floor(Math.random() * 2) + 1;
+  resultDiv.innerHTML = result === 1 ? "Heads" : "Tails";
 });
